42522f84effa7b2207452bd7be9d6ce56f772811
Parth Bhavsar <parthbhavsar@Parths-MacBook-Pro.local>
2018-02-16 12:05:28 -0500

    Added AE animation

--- app/src/main/java/io/zeroxp/splashanimation/MainActivity.java
+++ app/src/main/java/io/zeroxp/splashanimation/MainActivity.java
@@ -3,11 +3,17 @@ package io.zeroxp.splashanimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LottieAnimationView lottieAnimationView = (LottieAnimationView) findViewById(R.id.animationView_SplashAnimation);
+        
+
     }
 }
